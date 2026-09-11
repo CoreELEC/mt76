647c054310048bae2366550acd539371a5549d7b
Pro-me3us <promoe3us@gmail.com>
2026-09-11 18:28:15 -0400

    mt76-wifi: disable HE capabilities on 2.4GHz and 5GHz
    Problem: WiFi6 transfer speeds are slow (>150Mbps) and unstable compared to WiFi5
    
    Solution: Prevent the MT7921 driver from advertising 802.11ax/HE capabilities on 2.4GHz and 5GHz by removing the HE iftype data from those supported bands.
    
    This retains 2.4GHz and 5GHz connectivity while preventing Wi-Fi 6/802.11ax negotiation on those bands. 6GHz HE capabilities remain unchanged because the kernel requires HE capabilities for 6GHz band registration.

--- mt76/mt7921/main.c
+++ mt76/mt7921/main.c
@@ -216,8 +216,8 @@ void mt7921_set_stream_he_caps(struct mt7921_phy *phy)
 		n = mt7921_init_he_caps(phy, NL80211_BAND_2GHZ, data);
 
 		band = &phy->mt76->sband_2g.sband;
-		band->iftype_data = data;
-		band->n_iftype_data = n;
+		band->iftype_data = NULL;
+		band->n_iftype_data = 0;
 	}
 
 	if (phy->mt76->cap.has_5ghz) {
@@ -225,8 +225,8 @@ void mt7921_set_stream_he_caps(struct mt7921_phy *phy)
 		n = mt7921_init_he_caps(phy, NL80211_BAND_5GHZ, data);
 
 		band = &phy->mt76->sband_5g.sband;
-		band->iftype_data = data;
-		band->n_iftype_data = n;
+		band->iftype_data = NULL;
+		band->n_iftype_data = 0;
 
 		if (phy->mt76->cap.has_6ghz) {
 			data = phy->iftype[NL80211_BAND_6GHZ];
